00534734bbac418a07efba80cf4e9cf4eca03dd0
JANISA RIA <115360285+JanisaRia@users.noreply.github.com>
2025-01-25 16:04:42 +0530

    constants.py

--- constants.py
+++ constants.py
@@ -8,21 +8,21 @@ SENTENCE_TRANSFORMER_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 OPENAI_ADA_MODEL = "text-embedding-ada-002"
 
 #Batch size for generate embeddings
-BATCH_SIZE = 16
+BATCH_SIZE = batch_size
 
 # FAISS Index Parameters
-FAISS_TOP_K = 5  # Number of top results to retrieve
+FAISS_TOP_K = top_number  # Number of top results to retrieve
 
 # Chatbot Model
 GPT_MODEL = "gpt-3.5-turbo"
-GPT_MAX_TOKENS = 2500
+GPT_MAX_TOKENS = 250
 GPT_TEMPERATURE = 0.0  # Lower value for factual responses
 
 CHATBOT_PROMPT = """
             You are a banking assistant chatbot for Apollo Bank. 
             A user has this query:{user_query}.
-            And below are some of the relevant pieces of information, from the bank documentation for this query: {doc_context}. 
+            And below are some of the relevant pieces of information, from the bank documentation for this {doc_query{
             Consider the query and the info I have provided and give me a response that I can post as an answer to the user.
          """
 
-STREAMLIT_URL = "streamlit/url"
+STREAMLIT_URL = "streamlit-url"
